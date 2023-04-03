c2ab7a3803cf39fe9f4118f26d898404e8ed98d7
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-04-03 20:58:54 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ApiAlbum
+ Creando Api
---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function saveAlbum(){
+    const userID= document.getElementById("userID").value;
+    const title= document.getElementById("title").value;
+if(userID == 8 || title == "" ) {
+    return alert ("Los campos solicitados no pueden ser nulos")
+}
+console.log("Funciona")
+//POST
+//
+
+}
