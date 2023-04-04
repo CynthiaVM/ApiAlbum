4233fb542bc882e53678eb071a52730bbd41bbfd
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-04-03 22:55:12 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 function saveAlbum(){
     const userID= document.getElementById("userID").value;
     const title= document.getElementById("title").value;
-if(userID == 8 || title == "" ) {
+if(userID == 0 || title == "" ) {
     return alert ("Los campos solicitados no pueden ser nulos")
-}
+} //para verficar que no aparezcan vacios
 console.log("Funciona")
 //POST
-//
+ // https://jsonplaceholder.typicode.com/posts
+    // {
+    //     "userId" : 3,
+    //     "title": "Probando"
+    // }
 
 }
